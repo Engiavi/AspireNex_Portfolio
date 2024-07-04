35fca3b34692fbd2307508b00a3d7e789246fb50
“Avishank” <avishank.21b1541084@abes.ac.in>
2024-07-04 06:43:34 +0530

    refactor files -remove bug

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,15 +31,12 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          {/* <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </p> */}
           <TextGenerateEffect
-            words="Transforming Concepts Into Seamless User Experiences"
+            words="Bringing Ideas to Life Through Innovative Software Development"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <h1 className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <h1 className="text-center md:tracking-wider mb-4 text-sm md:text-xl lg:text-2xl">
             Hello! I&apos;m Avishank Agrahari, a {" "} 
             <span className="text-purple">
             <ReactTyped
